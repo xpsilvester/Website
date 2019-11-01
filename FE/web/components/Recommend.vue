3241aa750e9a85c640dcd367d3cb204b78c41d9a
xpsilvester <xpsilvester@gmail.com>
2019-11-01 16:42:56 +0800

    mod

---
+++ FE/web/components/Recommend.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="recommend">
+      
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+<style lange='scss'>
+    @import '~assets/scss/recommend.scss';
+</style>
--- FE/web/pages/Home.vue
+++ FE/web/pages/Home.vue
@@ -2,16 +2,19 @@
   <section class="home">
     <Slider width="100%" height="600px"  />
     <Activity />
+    <Recommend />
   </section>
 </template>
 
 <script>
 import Slider from '~/components/Slider.vue'
 import Activity from '~/components/Activity.vue'
+import Recommend from '~/components/Recommend.vue'
 export default {
   components: {
     Slider,
-    Activity
+    Activity,
+    Recommend
   }
 }
 </script>
