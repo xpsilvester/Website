68db3e8f9937c7bfd353c35c3fa308c67f84b1b1
xpsilvester <xpsilvester@gmail.com>
2020-04-09 17:04:39 +0800

    mod cestable

--- FE/cms/src/components/Cestable.vue
+++ FE/cms/src/components/Cestable.vue
@@ -1,121 +1,118 @@
 <!--表格组件 -->
 <template>
   <section id="cestable">
+    <!-- 查询表单 -->
     <div class="search">
-      <el-form :inline="true" :model="formInline" class="demo-form-inline">
-        <el-form-item label="姓名">
-          <el-input v-model="formInline.name" placeholder="请输入姓名"></el-input>
-        </el-form-item>
-        <el-form-item label="年龄">
-          <el-input v-model="formInline.age" placeholder="请输入年龄"></el-input>
-        </el-form-item>
-        <el-form-item label="性别">
-          <el-select v-model="formInline.region" placeholder="请选择性别">
+      <el-form :inline="true" :model="formData" class="demo-form-inline">
+        <el-form-item v-for="item in formOption" :key="item.prop" :label="item.label">
+          <el-input v-if="item.type == 'Input'" v-model="formData[item.prop]" :placeholder="item.placeholder"></el-input>
+          <el-select v-if="item.type == 'Select'" v-model="formData[item.prop]" :placeholder="item.placeholder">
             <el-option
-              v-for="item in formInline.genderOption"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
+              v-for="item2 in item.options"
+              :key="item2.value"
+              :label="item2.label"
+              :value="item2.value"
             ></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="onSubmit">查询</el-button>
-          <el-button type="primary" @click="onReset">重置</el-button>
+        <!-- 查询操作 -->
+        <el-form-item v-for="item in formOperation" :key="item.name">
+          <el-button :type="item.type" @click="item.handle">{{item.name}}</el-button>
         </el-form-item>
       </el-form>
     </div>
-    <div class="operation">
-      <el-button type="primary" @click="onAdd">新增</el-button>
+    <!-- 操作按钮 -->
+    <div class="operation" v-if="topOperation.length > 0">
+      <el-button v-for="item in topOperation" :key="item.name" :type="item.type" @click="item.handle">{{item.name}}</el-button>
     </div>
+    <!-- 数据列表 -->
     <div class="table-list">
       <el-table :data="tableData" height="350" border style="width: 100%">
-        <el-table-column prop="id" label="序号" width="180"></el-table-column>
-        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
-        <el-table-column prop="age" label="年龄" width="180"></el-table-column>
-        <el-table-column prop="gender" label="性别" width="180"></el-table-column>
-        <el-table-column prop="date" label="日期"></el-table-column>
+        <el-table-column v-for="item in tableCols" :key="item.prop" :prop="item.prop" :label="item.label" :width="item.width"></el-table-column>
         <el-table-column
           fixed="right"
           label="操作"
-          width="150">
+          width="150" v-if="tableOperation.length > 0">
           <template slot-scope="scope">
-            <el-button @click="handleClick(scope.row)" type="primary" size="small">编辑</el-button>
-            <el-button @click="handleClick(scope.row)" type="danger" size="small">删除</el-button>
+            <el-button @click="btn.handle(scope.row)" v-for="btn in tableOperation" :key="btn.name" :type="btn.type" size="small">{{btn.name}}</el-button>
           </template>
         </el-table-column>
       </el-table>
     </div>
+    <!-- 分页 -->
     <el-pagination style='display: flex;width:1200px;margin: 10px auto;justify-content: flex-end;height: 100%;align-items: center;'
-        @current-change="handleCurrentChange"
-        @size-change="handleSizeChange"
+        @current-change="pagination.handleCurrentChange"
+        @size-change="pagination.handleSizeChange"
         layout="total,sizes ,prev, pager, next,jumper"
-        :page-size="pagination.pageSize"
-        :current-page="pagination.pageNum"
-        :total="pagination.total"
+        :page-size="pagination.data.pageSize"
+        :current-page="pagination.data.pageNum"
+        :total="pagination.data.total"
     ></el-pagination>
   </section>
 </template>
 
 <script>
 export default {
-  props: {},
   data() {
     return {
-      formInline: {
-        name: "",
-        age: "",
-        gender: 0,
-        genderOption: [
-          {
-            label: "男",
-            value: 1
-          },
-          {
-            label: "女",
-            value: 2
-          }
-        ]
-      },
-      tableData: [],
-      pagination:{
-        pageSize:10,
-        pageNum:1,
-        total:7
-      }
+      
     };
+  },
+  props:{
+    tableData: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    tableCols: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    tableOperation: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    topOperation: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    formOption:{
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    formData: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    },
+    formOperation: {
+      type: Array,
+      default: function () {
+        return []
+      }
+    },
+    pagination: {
+      type: Object,
+      default: function () {
+        return {}
+      }
+    }
   },
   watch: {},
   methods: {
-    onSubmit() {
-      console.log("submit!");
-    },
-    onReset() {
-      console.log("reset!");
-    },
-    onAdd() {
-      console.log("add!");
-    },
-    handleClick(row){
-      console.log(row)
-    },
-    handleCurrentChange(){
-
-    },
-    handleSizeChange(){
-
-    }
+    
   },
   created(){
-    for(let i=1;i<20;i++){
-      this.tableData.push({
-        id: i,
-        name: '张三',
-        age: 26,
-        gender: '男',
-        date: "2016-05-03"
-      })
-    }
   }
 };
 </script>
--- FE/cms/src/pages/Table.vue
+++ FE/cms/src/pages/Table.vue
@@ -1,32 +1,118 @@
 <template>
   <div id="table">
     <h1>{{title}}</h1>
-    <Cestable />
+    <Cestable :tableData="tableData" 
+              :tableCols="tableCols" 
+              :tableOperation="[
+                {type:'primary',name:'编辑',handle:handleClick},
+                {type:'danger',name:'删除',handle:handleClick}              
+              ]"
+              :topOperation="[
+                {type:'primary',name:'新增',handle:onAdd}
+              ]"
+              :formData="searchData"
+              :formOption="searchForm"
+              :formOperation="[
+                {type:'primary',name:'查询',handle:onSearch},
+                {type:'primary',name:'重置',handle:onReset}
+              ]"
+              :pagination="{data:pagination,handleCurrentChange,handleSizeChange}" />
   </div>
 </template>
 
 <script>
-import Cestable from '../components/Cestable.vue'
+import Cestable from "../components/Cestable.vue";
 
 export default {
-    name:'Table',
-    data(){
-        return {
-            title: 'table',
+  name: "Table",
+  data() {
+    return {
+      title: "table",
+      tableData:[],
+      tableCols:[
+        {
+          prop: 'id',
+          label: '序号',
+          width: 100
+        },
+        {
+          prop: 'name',
+          label: '姓名',
+          width: 180
+        },
+        {
+          prop: 'age',
+          label: '年龄',
+          width: 180
+        },
+        {
+          prop: 'gender',
+          label: '性别',
+          width: 180
+        },
+        {
+          prop: 'date',
+          label: '日期',
+          width: 'auto'
         }
+      ],
+      searchForm:[
+        {type:'Input',label:'姓名',prop:'name',width:'180px',placeholder:'请输入姓名'},
+        {type:'Input',label:'年龄',prop:'age',width:'180px',placeholder:'请输入年龄'},
+        {type:'Select',label:'性别',prop:'gender',width:'180px',options:[{label: "男",value: 1},{label: "女",value: 2}],placeholder:'请选择性别'}
+      ],
+      searchData:{
+        name: "",
+        age: "",
+        gender: ''
+      },
+      pagination:{
+        pageSize:10,
+        pageNum:1,
+        total:7
+      }
+    };
+  },
+  components: {
+    Cestable
+  },
+  methods: {
+    handleClick(row){
+      console.log(row)
     },
-    components:{
-      Cestable
+    onAdd(){
+      console.log('add!')
     },
-    methods:{
-      
+    onSearch(){
+      console.log('search',this.searchData)
     },
-    created(){
-      
+    onReset(){
+      this.searchData = {
+        name: "",
+        age: "",
+        gender: ''
+      }
+    },
+    handleCurrentChange(data){
+      console.log(data)
+    },
+    handleSizeChange(data){
+      console.log(data)
     }
-}
+  },
+  created() {
+    for (let i = 1; i < 20; i++) {
+      this.tableData.push({
+        id: i,
+        name: "张三",
+        age: 26,
+        gender: "男",
+        date: "2016-05-03"
+      });
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
